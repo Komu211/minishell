e25adfae1e87244891594a07e8ddcb26659d2e1f
Konrad Muhlbauer <75761278+Komu211@users.noreply.github.com>
2025-01-10 17:42:58 +0100

    Refactor: Name change + added to header

--- includes/builtins.h
+++ includes/builtins.h
@@ -6,7 +6,7 @@
 /*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/13 18:31:53 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2024/12/13 18:50:09 by kmuhlbau         ###   ########.fr       */
+/*   Updated: 2025/01/10 17:41:49 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,9 @@
 
 # include <libft.h>
 # include <unistd.h>
+#include "env.h"
 
 int	builtin_echo(char **args);
+int	builtin_env(t_list *env_list);
 
 #endif
--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,13 +6,13 @@
 /*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 17:36:30 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2025/01/10 17:41:05 by kmuhlbau         ###   ########.fr       */
+/*   Updated: 2025/01/10 17:41:58 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtins.h"
 
-int	env_command(t_list *env_list)
+int	builtin_env(t_list *env_list)
 {
 	t_env	*env;
 
