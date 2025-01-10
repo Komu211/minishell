3af00155f2604d4d1fc190f94603224b90f9a206
Konrad Muhlbauer <75761278+Komu211@users.noreply.github.com>
2025-01-10 16:53:41 +0100

    Norm fix and issue with wrong path being returned

--- src/execution/execute_utils.c
+++ src/execution/execute_utils.c
@@ -6,7 +6,7 @@
 /*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 10:36:20 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2025/01/10 16:34:27 by kmuhlbau         ###   ########.fr       */
+/*   Updated: 2025/01/10 16:52:15 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,17 @@ char	*get_command_path(char *command, t_list *env_list)
 	int		i;
 	char	*tmp;
 
+	if (access(command, F_OK) == 0)
+		return (gc_strdup(command));
 	env_path = get_env_value("PATH", env_list);
 	if (!env_path)
 		return (NULL);
-	printf("1\n");
 	path = gc_split_at(env_path, ':');
-	printf("2\n");
 	i = 0;
 	while (path[i])
 	{
-		printf("path[i]: %s\n", path[i]);
 		tmp = gc_strjoin(path[i], "/");
 		command_path = gc_strjoin(tmp, command);
-		printf("command_path: %s\n", command_path);
 		gc_free(tmp);
 		if (access(command_path, F_OK) == 0)
 			return (gc_split_free(&path), command_path);
