a397a6874601324c8c031dac8039107aeb1ac774
Konrad Muhlbauer <75761278+Komu211@users.noreply.github.com>
2025-01-11 18:57:21 +0100

    Added forgotten files

---
+++ src/execution/execute_redirection.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_redirection.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 18:34:10 by kmuhlbau          #+#    #+#             */
+/*   Updated: 2025/01/11 18:54:53 by kmuhlbau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execution.h"
+
+int	execute_redirection(t_minishell *mini, t_ast_node *ast)
+{
+	(void)mini;
+	(void)ast;
+	return (0);
+}
---
+++ src/utils/is_redirection.c
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_redirection.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 18:30:46 by kmuhlbau          #+#    #+#             */
+/*   Updated: 2025/01/11 18:31:40 by kmuhlbau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "utils.h"
+
+int	is_redirection(t_token_type token)
+{
+	return (token == TOKEN_REDIRECT_IN || token == TOKEN_REDIRECT_OUT
+		|| token == TOKEN_REDIRECT_OUT_APPEND
+		|| token == TOKEN_REDIRECT_HERE_DOC);
+}
