581630504386beb7f5edc6a7d72508876216a4cb
Ondřej Běhávka <obehavka@student.42heilbronn.de>
2025-01-12 13:34:26 +0100

    Removed free debug print

--- src/wrapper/gc_free.c
+++ src/wrapper/gc_free.c
@@ -6,7 +6,7 @@
 /*   By: obehavka <obehavka@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 12:57:34 by obehavka          #+#    #+#             */
-/*   Updated: 2025/01/11 16:52:36 by obehavka         ###   ########.fr       */
+/*   Updated: 2025/01/12 13:30:49 by obehavka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	gc_free(void *ptr)
 {
 	if (!ptr)
 		return ;
-	printf("Freeing pointer: %p\n", ptr);
 	if (!garbage_collector_remove(ptr))
 		error_handler("Possible double free:\n"
 						"Trying to gc_free pointer that was not gc_malloced, "
