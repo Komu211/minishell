c05186c7676cd616886d18034f8a5a08d1323977
Ondřej Běhávka <obehavka@student.42heilbronn.de>
2025-01-19 11:21:56 +0100

    Made fnuction const

--- includes/env.h
+++ includes/env.h
@@ -6,7 +6,7 @@
 /*   By: obehavka <obehavka@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 15:18:25 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2025/01/19 11:20:31 by obehavka         ###   ########.fr       */
+/*   Updated: 2025/01/19 11:21:10 by obehavka         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 void	env_init(t_minishell *mini, char **envp);
 void	env_destroy(void *env);
 void	env_empty(t_list **env);
-char	*get_env_value(char *key, t_list *env_list);
+const char	*get_env_value(char *key, t_list *env_list);
 void	env_add(t_minishell *mini, char *key, char *value);
 void	env_set(t_minishell *mini, char *key, char *value);
 
