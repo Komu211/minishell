54352d8d1948e7083c72c45bd43f514d5b202814
Konrad Muhlbauer <75761278+Komu211@users.noreply.github.com>
2025-02-02 17:37:44 +0100

    Main norm conform

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: obehavka <obehavka@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 15:06:57 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2025/02/02 13:54:19 by obehavka         ###   ########.fr       */
+/*   Updated: 2025/02/02 17:34:29 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,11 @@ void	print_welcome(void)
 	ft_putendl_fd("│   / __ \\/ //_/__  ____ ___  __ / ___// /_  ___  / / /│",
 		1);
 	ft_putendl_fd("│  / / / / ,< / _ \\/ __ `/ / / / \\__ \\/ __ \\/ _ \\/ / "
-					"/ │",
-					1);
+		"/ │", 1);
 	ft_putendl_fd("│ / /_/ / /| /  __/ /_/ / /_/ / ___/ / / / /  __/ / /  │",
 		1);
 	ft_putendl_fd("│ \\____/_/ |_\\___/\\__,_/\\__, / /____/_/ /_/\\___/_/_"
-					"/   │",
-					1);
+		"/   │", 1);
 	ft_putendl_fd("│                      /____/                          │",
 		1);
 	ft_putendl_fd("└──────────────────────────────────────────────────────┘",
@@ -36,20 +34,52 @@ void	print_welcome(void)
 	ft_putstr_fd("\033[0m\n", 1);
 }
 
-void	mini_init(char **envp, t_minishell **mini)
+static void	handle_heredocs(t_minishell *mini, t_heredoc **heredocs,
+		int *heredoc_counter)
 {
-	*mini = gc_calloc(1, sizeof(t_minishell));
-	*get_mini() = *mini;
-	(*mini)->pwd = gc_getcwd(NULL);
-	if (!(*mini)->pwd)
-		error_handler("getcwd failed", 1);
-	(*mini)->hist_file = gc_strjoin((*mini)->pwd, "/.minishell_history");
-	if (!(*mini)->hist_file)
-		error_handler("ft_strjoin failed", 1);
-	env_init(*mini, envp);
-	(*mini)->error = 0;
-	(*mini)->exit_status = 0;
-	(*mini)->old_pwd = NULL;
+	*heredocs = NULL;
+	if (mini->ast && collect_heredocs_from_node(mini->ast, heredocs,
+			heredoc_counter) && read_heredocs(*heredocs, mini))
+	{
+		apply_heredocs_to_ast(mini->ast, *heredocs);
+	}
+}
+
+static char	*get_user_input(char *prompt)
+{
+	char	*user_in;
+	char	*line;
+
+	if (isatty(STDIN_FILENO))
+		user_in = readline(prompt);
+	else
+	{
+		line = get_next_line(STDIN_FILENO);
+		if (line)
+		{
+			user_in = ft_strtrim(line, "\n");
+			free(line);
+		}
+		else
+			user_in = NULL;
+	}
+	return (user_in);
+}
+
+static void	process_command(t_minishell *mini, char *user_in,
+		t_heredoc **heredocs, int *heredoc_counter)
+{
+	if (*user_in)
+	{
+		ast_init(&mini->ast, user_in, mini);
+		handle_heredocs(mini, heredocs, heredoc_counter);
+		if (mini->ast)
+		{
+			mini->exit_status = execute_ast(mini, mini->ast);
+			mini->ast = ast_empty(mini->ast);
+		}
+		add_history(user_in);
+	}
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -57,57 +87,25 @@ int	main(int argc, char **argv, char **envp)
 	t_minishell	*mini;
 	char		*user_in;
 	char		*prompt;
-	char		*line;
 	t_heredoc	*heredocs;
 	int			heredoc_counter;
 
-	(void)argc;
-	(void)argv;
-	mini_init(envp, &mini);
+	mini_init(envp, &mini, argc, argv);
 	signal_setup(mini);
 	heredoc_counter = 0;
 	heredocs = NULL;
 	while (42)
 	{
 		prompt = gc_strjoin(mini->pwd, " > ");
-		if (isatty(STDIN_FILENO))
-			user_in = readline(prompt);
-		else
-		{
-			line = get_next_line(STDIN_FILENO);
-			if (line)
-			{
-				user_in = ft_strtrim(line, "\n");
-				free(line);
-			}
-			else
-				user_in = NULL;
-		}
+		user_in = get_user_input(prompt);
 		if (!user_in)
 			break ;
 		garbage_collector_add(user_in);
-		if (*user_in) // Only process non-empty input
-		{
-			heredocs = NULL;
-			ast_init(&mini->ast, user_in, mini);
-			if (mini->ast && collect_heredocs_from_node(mini->ast, &heredocs,
-					&heredoc_counter) && read_heredocs(heredocs, mini))
-			{
-				// printf("\nCommand entered: %s\n", user_in);
-				// debug_ast(mini.ast);
-				apply_heredocs_to_ast(mini->ast, heredocs);
-				mini->exit_status = execute_ast(mini, mini->ast);
-				// Free previous AST before next iteration
-				mini->ast = ast_empty(mini->ast);
-			}
-			add_history(user_in); // Add command to readline history
-		}
+		process_command(mini, user_in, &heredocs, &heredoc_counter);
 		gc_free(user_in);
 		gc_free(prompt);
 		fd_collector_empty();
 		empty_heredoc(&heredocs);
-		// printf("garbage size: %zu\n", garbage_collector_size());
-		// garbage_collector_print();
 	}
 	cleanup_main(mini);
 	return (mini->exit_status);
